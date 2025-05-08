a50828add9f7a0383525ca8c31e050604e233163
afzal <iammgafsalk86@gmail.com>
2025-05-08 11:55:15 +0530

    data loading

--- transformer/csv_loader.py
+++ transformer/csv_loader.py
@@ -1,6 +1,7 @@
 import torch
 import pandas as pd
 from torch.utils.data import Dataset
+from tokenizers import Tokenizer
 
 
 class Translation(Dataset):
@@ -9,14 +10,18 @@ class Translation(Dataset):
         self.df = pd.read_csv('/home/afsal/Downloads/Torch/data/translation_train.csv')
         self.src = self.df['english']
         self.tgt = self.df['german']
+        self.src_token = Tokenizer.from_file('/home/afsal/Downloads/Torch/transformer/english.json')
+        self.tgt_token = Tokenizer.from_file('/home/afsal/Downloads/Torch/transformer/german.json')
 
 
     def __getitem__(self,index):
 
         source = self.src.iloc[index].strip()
         target = self.tgt.iloc[index].strip()
+        src_id = self.src_token.encode(source).ids
+        tgt_id = self.tgt_token.encode(target).ids
 
-        return source,target
+        return src_id,tgt_id
 
 translates = Translation()
 
